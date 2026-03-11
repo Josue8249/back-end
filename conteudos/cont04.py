da583406e4250b6a7ee59a01f78ed1f021f3127e
Josue8249 <josue.sertao.lima@escola.pr.gov.br>
2026-03-11 11:44:36 -0300

    Create cont04.py

---
+++ conteudos/cont04.py
@@ -0,0 +1,13 @@
+O + é utilizado como operador de adição
+Programaono (EM 12
+O - é utilizado como sinal de subtração
+Bence De Durtes II-2 Ano
+O * é utilizado para fazer a multiplicação
+Programece Front-Enit-2
+/ é utilizada para fazer a divisão
+MANG TH
+** é utilizado para fazer a exponenciação
+2MB Martm
+// é utilizado para fazer a divisão inteira
+MATHE
+% é utilizada para o resto da divisão
