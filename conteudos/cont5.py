3aa3a8ebe4f13ec05e7fe7c2362b12b5495d0299
Josue8249 <josue.sertao.lima@escola.pr.gov.br>
2026-03-16 10:45:03 -0300

    Create cont5.py

---
+++ conteudos/cont5.py
@@ -0,0 +1,17 @@
+#Definição: é a ordem na qual os operadores serão executados
+
+#1 Expressões dentro de parenteses
+
+#2 Em seguida vem as potenciações
+
+#3 Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro
+
+#4 E por último a soma e a subtração
+
+#5+3*2==
+
+#3^ * 5 + 4 ^ 2 =
+
+#3^ * (5 + 4) ^ 2 =
+
+#O Python é utilizado em cálculos científicos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
