6081eacbc0793c88984a8f36437bce23a6a4a07e
Josue8249 <josue.sertao.lima@escola.pr.gov.br>
2026-03-23 10:43:58 -0300

    Update ex08.py

--- exercicios/ex08.py
+++ exercicios/ex08.py
@@ -1,3 +1,3 @@
 preco = float(input('Qual é o preço do produto? R$ '))
-novo_preco = preco * 0.95
+novo_preco = preco - 18.70 (preco * 0.05)
 print(f'O produto que custava R${preco:.2f}, na promoção com desconto de 5% vai custar R${novo_preco:.2f}')
